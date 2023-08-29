4107c7c5c4af7c33b83d8d7c84ecbaf385e4cd24
Ausitn <dudasaus@umich.edu>
2023-08-29 10:28:18 -0600

    leaderboard page

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -3,41 +3,40 @@ import viteLogo from "/vite.svg";
 import "./App.scss";
 import { useAtom } from "jotai";
 import { countAtom } from "./atoms";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 console.log(import.meta.env);
 
 function App() {
-  const [count, setCount] = useAtom(countAtom);
+  const [loading, setLoading] = useState(true);
+  const [leaderboard, setLeaderboard] = useState<any[]>([]);
 
-  async function testFetch() {
-    const res = await fetch("/api/leaderboard");
-    const parsed = await res.json();
-    console.log(parsed);
+  useEffect(() => {
+    fetch("/api/leaderboard")
+      .then((res) => res.json())
+      .then((body) => {
+        setLeaderboard(body);
+        setLoading(false);
+      });
+  }, []);
+
+  function renderLeaderboard() {
+    if (loading) return <p>Loading...</p>;
+    return (
+      <ol>
+        {leaderboard.map((entry) => (
+          <li>
+            {entry.username} - {entry.fish}
+          </li>
+        ))}
+      </ol>
+    );
   }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          <button onClick={() => testFetch()}>Fetch</button>
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Discord Fishing Leaderboard</h1>
+      {renderLeaderboard()}
     </>
   );
 }
